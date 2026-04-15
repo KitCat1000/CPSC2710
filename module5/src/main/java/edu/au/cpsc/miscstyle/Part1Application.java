307aa7500f4cf76416a288840f489e7ef672a420
Nicole Tressler <ironraptor500@gmail.com>
2026-04-14 22:55:14 -0500

    Flight app part1

---
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
@@ -0,0 +1,33 @@
+/*
+ * Assignment: Part 1 - Flight Designator App
+ * Author: Nicole Tressler
+ * Auburn Email: nit0005@auburn.edu
+ * Date: April 14, 2026
+ * Description: A JavaFX application demonstrating CSS styling with various UI controls
+ */
+
+
+package edu.au.cpsc.miscstyle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Part1Application extends Application {
+
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(Part1Application.class.getResource("../../../../../../../../../../Downloads/part1.fxml"));
+    Scene scene = new Scene(fxmlLoader.load());
+    stage.setTitle("Nicole Tressler's Flight Designator App");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
+}
---
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Assignment: Part 1 - Flight Designator App
+ * Author: Nicole Tressler
+ * Auburn Email: nit0005@auburn.edu
+ * Date: April 14, 2026
+ * Description: A JavaFX application demonstrating CSS styling with various UI controls
+ */
+
+package edu.au.cpsc.miscstyle;
+
+public class Part1Controller {
+
+}
