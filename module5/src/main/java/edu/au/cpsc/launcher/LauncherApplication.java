8567fd3e520da223a7cf117cead5463054910fa4
Nicole Tressler <ironraptor500@gmail.com>
2026-04-19 17:38:58 -0500

    part2

--- module5/src/main/java/edu/au/cpsc/launcher/LauncherApplication.java
+++ module5/src/main/java/edu/au/cpsc/launcher/LauncherApplication.java
@@ -6,10 +6,11 @@
  * Description: A launcher application with a 2x2 grid of buttons to access different modules
  */
 
-        package edu.au.cpsc.launcher;
+package edu.au.cpsc.launcher;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -17,11 +18,22 @@ import java.io.IOException;
 public class LauncherApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LauncherApplication.class.getResource("launcher-app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500); // Adjust size as needed
+        FXMLLoader loader = new FXMLLoader(
+                LauncherApplication.class.getResource("launcher-app.fxml")
+        );
+
+        if (loader.getLocation() == null) {
+            System.err.println("ERROR: launcher-app.fxml not found!");
+            System.err.println("Make sure the file is at: resources/edu/au/cpsc/launcher/launcher-app.fxml");
+            throw new IOException("launcher-app.fxml not found");
+        }
+
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 500);
 
         // Load the CSS file
-        scene.getStylesheets().add(LauncherApplication.class.getResource("style/main.css").toExternalForm());
+        String css = LauncherApplication.class.getResource("style/main.css").toExternalForm();
+        scene.getStylesheets().add(css);
 
         stage.setTitle("Flight Designator Launcher");
         stage.setScene(scene);
